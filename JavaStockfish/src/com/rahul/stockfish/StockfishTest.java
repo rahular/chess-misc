dc0b6a50e7b8f7e9793e7149930af5833aadd238
rahular <rahulife007@gmail.com>
2014-02-02 01:02:50 +0530

    added drawBoard, legalMoves

--- JavaStockfish/src/com/rahul/stockfish/StockfishTest.java
+++ JavaStockfish/src/com/rahul/stockfish/StockfishTest.java
@@ -1,23 +1,35 @@
 package com.rahul.stockfish;
 
-
 public class StockfishTest {
 	public static void main(String[] args) {
 		Stockfish client = new Stockfish();
+		String FEN = "8/6pk/8/1R5p/3K3P/8/6r1/8 b - - 0 42";
+
 		// initialize and connect to engine
 		if (client.startEngine()) {
 			System.out.println("Engine has started..");
 		} else {
 			System.out.println("Oops! Something went wrong..");
 		}
+
 		// send commands manually
 		client.sendCommand("uci");
+
 		// receive output dump
 		System.out.println(client.getOutput(0));
+
 		// get the best move for a position with a given think time
-		System.out.println(client.getBestMove(
-				"8/6pk/8/1R5p/3K3P/8/6r1/8 b - - 0 42", 1000));
+		System.out.println("Best move : " + client.getBestMove(FEN, 100));
+
+		// get all the legal moves from a given position
+		System.out.println("Legal moves : " + client.getLegalMoves(FEN));
+
+		// draw board from a given position
+		System.out.println("Board state :");
+		client.drawBoard(FEN);
+
 		// stop the engine
+		System.out.println("Stopping engine..");
 		client.stopEngine();
 	}
 }
